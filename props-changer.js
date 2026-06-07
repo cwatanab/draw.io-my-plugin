94b5ca511a99d51f788ac73eb63f654461f8b469
Chihiro WATANABE <chihiro.watanabe@live.jp>
2026-06-07 12:03:42 +0900

    Add 'prohibit movement' property to props-changer

--- props-changer.js
+++ props-changer.js
@@ -94,6 +94,15 @@
                     { value: '1', label: '許可' },
                     { value: '0', label: '禁止' }
                 ]
+            },
+            {
+                key: 'movable',
+                label: '移動を禁止',
+                defaultOff: true,
+                values: [
+                    { value: '1', label: '許可' },
+                    { value: '0', label: '禁止' }
+                ]
             }
         ]
     };
